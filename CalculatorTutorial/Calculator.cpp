1596c8ddc4c6d7fe74f3bed7c4f66f1c43aa1401
Luis M. Melendez <cogane7955@gmail.com>
2020-04-07 15:06:08 -0500

    Agregar archivos de proyecto.

---
+++ CalculatorTutorial/Calculator.cpp
@@ -0,0 +1,23 @@
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    default:
+        return 0.0;
+    }
+}
+
+double Calculator::evaluate()
+{
+	return 0.0;
+}
